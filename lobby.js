af6d820f5065c30ccba391f08b5fb9ebf8614849
crsph2 <crsph2@gmail.com>
2026-08-27 21:35:40 -0400

    Update lobby.js

--- lobby.js
+++ lobby.js
@@ -14,8 +14,8 @@ const ITEMS = {
         { id: 'sigma', nombre: 'Sigma (Σ)', categoria: 'simbolo', precio: 1000, pos: 'bottom-right' },
         { id: 'infinito', nombre: 'Infinito (∞)', categoria: 'simbolo', precio: 1000, pos: 'center-left' },
         { id: 'delta', nombre: 'Delta (Δ)', categoria: 'simbolo', precio: 1000, pos: 'center-right' },
-        { id: 'theta', nombre: 'Theta (θ)', categoria: 'simbolo', precio: 1000, pos: 'top-center' },    // corregido
-        { id: 'suma_frac', nombre: 'Fracción', categoria: 'simbolo', precio: 1000, pos: 'bottom-center' } // corregido
+        { id: 'theta', nombre: 'Theta (θ)', categoria: 'simbolo', precio: 1000, pos: 'top-center' },
+        { id: 'suma_frac', nombre: 'Fracción', categoria: 'simbolo', precio: 1000, pos: 'bottom-center' }
     ]
 };
 
